713fffb4de28077e9ac2e7adedff1c790fc5c354
xxiMiaxx <lam3h-sa@live.co.uk>
2017-10-22 12:20:02 +0300

    edit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -47,6 +47,7 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         ggg
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
